372b23abe1f68b539ec09750cec2fc9796dbcd5b
falkin <valentin.bourqui@gmail.com>
2016-08-18 03:48:34 +0200

    fix bug torrentz

--- couchpotato/core/media/movie/providers/torrent/torrentz.py
+++ couchpotato/core/media/movie/providers/torrent/torrentz.py
@@ -5,7 +5,7 @@ from couchpotato.core.media.movie.providers.base import MovieProvider
 
 log = CPLog(__name__)
 
-autoload = 'Torrentz2'
+autoload = 'Torrentz'
 
 
 class Torrentz(MovieProvider, Base):
